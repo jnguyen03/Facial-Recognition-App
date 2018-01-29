86ff3d2c05e453aeac73db76338135c0358e3317
Jim Nguyen <jim.nguyen03@gmail.com>
2018-01-29 14:20:29 -0800

    added backend functionality

--- src/App.js
+++ src/App.js
@@ -34,8 +34,31 @@ class App extends Component {
       imageUrl: '',
       box: {},
       route: 'signin',
-      isSignedIn: false
+      isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: ''
+      }
     }
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3000')
+      .then(response => response.json())
+      .then(console.log)
+  }
+
+  loadUser = (data) => {
+    this.setState({user: {
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        entries: data.entries,
+        joined: data.joined
+    }})
   }
 
   calculateFaceLocation = (data) => {
@@ -64,7 +87,22 @@ class App extends Component {
     app.models.predict(
       Clarifai.FACE_DETECT_MODEL,
       this.state.input)
-    .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+    .then(response => {
+      if (response) {
+        fetch('http://localhost:3000/image', {
+          method: 'put',
+          headers: {'Content-Type': 'application/json',
+          body: JSON.stringify({
+            id: this.state.user.id,
+          })}
+        })
+          .then(response => response.json())
+          .then(count => {
+            this.setState(Object.assign(this.state.user, { entries: count }))
+          })
+      }
+      this.displayFaceBox(this.calculateFaceLocation(response))
+      })
     .catch(err => console.log(err));
   }  
   
@@ -87,15 +125,15 @@ class App extends Component {
         { this.state.route === 'home'
           ? <div>
             <Logo />
-            <Rank />
+            <Rank name={this.state.user.entries} entries={this.state.user.entries} />
             <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
             <FaceRecognition box={this.state.box}imageUrl={this.state.imageUrl} />
           </div>
           :
             (
               this.state.route === 'signin'
-                ? <Signin onRouteChange={this.onRouteChange} />
-                : <Register onRouteChange={this.onRouteChange} />
+                ? <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+                : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
             ) 
         }
       </div>
--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 
-const Rank = () => {
+const Rank = ({name, entries}) => {
 	return (
 		<div>
 			<div className="white f3">
