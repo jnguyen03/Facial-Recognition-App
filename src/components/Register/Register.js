b2dc4ad6c94b261b0e454bb5573036e4269dce21
Jim <Jim@Jims-MBP.attlocal.net>
2018-01-30 16:33:13 -0800

    added heroku url

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
 	}
 
 	onSubmitSignIn = () => {
-		fetch('http://localhost:3000/register', { 
+		fetch('https://git.heroku.com/still-reaches-69644.git/register', { 
 			method: 'post',
 			headers: {'Content-Type': 'application/json'},
 			body: JSON.stringify({
--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
 	}
 
 	onSubmitSignIn = () => {
-		fetch('http://localhost:3000/signin', { 
+		fetch('https://git.heroku.com/still-reaches-69644.git/signin', { 
 			method: 'post',
 			headers: {'Content-Type': 'application/json'},
 			body: JSON.stringify({
